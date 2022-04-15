5a4fa1898dc4116d337abdba6776a9ab8ade916f
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-15 11:44:12 -0400

    commit tree 3ceee46f304b2a2ace712f06749be8f012a28bb3

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>If you want to try something different, you can make ramen yakisoba – boil your ramen for a few minutes, then drain it and fry it up with veggies and meat! FYI, you can make a really good yakisoba umami sauce with ketchup, Worcestershire sauce, soy sauce, and Sriracha! Get the full recipe here.</p>
+      <img src={image} alt="I made this"></img>
+    </div>
+  )
 }
 
 export default About;
